58cfeb906ee140707f31805e14b0447a5216c354
annyang <50082403+annyang1963@users.noreply.github.com>
2023-04-18 14:38:52 -0700

    Add files via upload

---
+++ ml/model.py
@@ -0,0 +1,127 @@
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+from ml.data import process_data
+# TODO: add necessary import
+
+# Optional: implement hyperparameter tuning.
+def train_model(X_train, y_train):
+    """
+    Trains a machine learning model and returns it.
+
+    Inputs
+    ------
+    X_train : np.array
+        Training data.
+    y_train : np.array
+        Labels.
+    Returns
+    -------
+    model
+        Trained machine learning model.
+    """
+   # TODO: implement the function
+    pass
+
+
+def compute_model_metrics(y, preds):
+    """
+    Validates the trained machine learning model using precision, recall, and F1.
+
+    Inputs
+    ------
+    y : np.array
+        Known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+    Returns
+    -------
+    precision : float
+    recall : float
+    fbeta : float
+    """
+    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
+    precision = precision_score(y, preds, zero_division=1)
+    recall = recall_score(y, preds, zero_division=1)
+    return precision, recall, fbeta
+
+
+def inference(model, X):
+    """ Run model inferences and return the predictions.
+
+    Inputs
+    ------
+    model : ???
+        Trained machine learning model.
+    X : np.array
+        Data used for prediction.
+    Returns
+    -------
+    preds : np.array
+        Predictions from the model.
+    """
+    # TODO: implement the function
+    pass
+
+def save_model(model, path):
+    """ Serializes model to a file.
+
+    Inputs
+    ------
+    model
+        Trained machine learning model or OneHotEncoder.
+    path : str
+        Path to save pickle file.
+    """
+    # TODO: implement the function
+    pass
+
+def load_model(path):
+    """ Loads pickle file from `path` and returns it."""
+    # TODO: implement the function
+    pass
+
+
+def performance_on_categorical_slice(
+    data, column_name, slice_value, categorical_features, label, encoder, lb, model
+):
+    """ Computes the model metrics on a slice of the data specified by a column name and
+
+    Processes the data using one hot encoding for the categorical features and a
+    label binarizer for the labels. This can be used in either training or
+    inference/validation.
+
+    Inputs
+    ------
+    data : pd.DataFrame
+        Dataframe containing the features and label. Columns in `categorical_features`
+    column_name : str
+        Column containing the sliced feature.
+    slice_value : str, int, float
+        Value of the slice feature.
+    categorical_features: list
+        List containing the names of the categorical features (default=[])
+    label : str
+        Name of the label column in `X`. If None, then an empty array will be returned
+        for y (default=None)
+    encoder : sklearn.preprocessing._encoders.OneHotEncoder
+        Trained sklearn OneHotEncoder, only used if training=False.
+    lb : sklearn.preprocessing._label.LabelBinarizer
+        Trained sklearn LabelBinarizer, only used if training=False.
+    model : ???
+        Model used for the task.
+
+    Returns
+    -------
+    precision : float
+    recall : float
+    fbeta : float
+
+    """
+    # TODO: implement the function
+    X_slice, y_slice, _, _ = process_data(
+        # your code here
+        # for input data, use data in column given as "column_name", with the slice_value 
+        # use training = False
+    )
+    preds = # your code here to get prediction on X_slice using the inference function
+    precision, recall, fbeta = compute_model_metrics(y_slice, preds)
+    return precision, recall, fbeta
---
+++ test_ml.py
@@ -0,0 +1,26 @@
+@pytest.fixture(scope="session")
+# TODO: implement the first test. Change the function name and input as needed
+def test_one():
+    """
+    # add description for the first test
+    """
+    # Your code here
+    pass
+
+
+# TODO: implement the second test. Change the function name and input as needed
+def test_two():
+    """
+    # add description for the second test
+    """
+    # Your code here
+    pass
+
+
+# TODO: implement the third test. Change the function name and input as needed
+def test_three():
+    """
+    # add description for the third test
+    """
+    # Your code here
+    pass
