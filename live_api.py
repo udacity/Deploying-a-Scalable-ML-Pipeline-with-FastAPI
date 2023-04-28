84d909e33ff95610f54966b84c20edc4e6c8b103
annyang <50082403+annyang1963@users.noreply.github.com>
2023-04-28 09:35:40 -0700

    Add files via upload

---
+++ live_api.py
@@ -0,0 +1,38 @@
+import json
+
+import requests
+
+# TODO: send a GET using the URL http://127.0.0.1:8000
+r = # Your code here
+
+# TODO: print the status code
+# print()
+# TODO: print the welcome message
+# print()
+
+
+
+data = {
+    "age": 37,
+    "workclass": "Private",
+    "fnlgt": 178356,
+    "education": "HS-grad",
+    "education-num": 10,
+    "marital-status": "Married-civ-spouse",
+    "occupation": "Prof-specialty",
+    "relationship": "Husband",
+    "race": "White",
+    "sex": "Male",
+    "capital-gain": 0,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "United-States",
+}
+
+# TODO: send a POST using the data above
+r = # Your code here
+
+# TODO: print the status code
+# print()
+# TODO: print the result
+# print()
