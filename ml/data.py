0b694d972e2e6387413d8339cecf3ad487cad136
DRroth76 <david.randall.roth97@gmail.com>
2024-10-16 18:38:31 -0500

    Resolving code using black

--- ml/data.py
+++ ml/data.py
@@ -3,8 +3,7 @@ from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
 
 
 def process_data(
-    X, categorical_features=[], label=None, training=True,
-    encoder=None, lb=None
+    X, categorical_features=[], label=None, training=True, encoder=None, lb=None
 ):
     """Process the data used in the machine learning pipeline.
 
