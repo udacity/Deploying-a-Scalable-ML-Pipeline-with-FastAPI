4bd2161c9de5f219355d555bd7ce37a29a81bdb1
Kyungwon Chun <ruddyscent@gmail.com>
2024-11-07 13:05:06 +0900

    Fix syntax error in model.py and update placeholder assignment for prediction variable
    - Corrected indentation in `train_model` function's TODO comment.
    - Assigned `None` as a placeholder for `preds` variable in `performance_on_categorical_slice` function to resolve syntax error during GitHub Action run.

--- ml/model.py
+++ ml/model.py
@@ -19,7 +19,7 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-   # TODO: implement the function
+    # TODO: implement the function
     pass
 
 
@@ -123,6 +123,6 @@ def performance_on_categorical_slice(
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
     )
-    preds = # your code here to get prediction on X_slice using the inference function
+    preds = None # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
