cb89f3564895f89a227c6c6cb1e4ac3a3546f159
efransen0828 <48602466+efransen0828@users.noreply.github.com>
2025-01-04 12:18:09 -0700

    Update local_api.py

--- local_api.py
+++ local_api.py
@@ -1,16 +1,13 @@
-import json
-
 import requests
 
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+r = requests.get("http://127.0.0.1:8000")
 
 # TODO: print the status code
-# print()
+print("GET Request Status Code:", r.status_code)
+
 # TODO: print the welcome message
-# print()
-
-
+print("GET Request Response:", r.text)
 
 data = {
     "age": 37,
@@ -30,9 +27,10 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post("http://127.0.0.1:8000/predict", json=data)
 
 # TODO: print the status code
-# print()
+print("POST Request Status Code:", r.status_code)
+
 # TODO: print the result
-# print()
+print("POST Request Response:", r.json())
